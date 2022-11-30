88723a9fc5b62e92793856eccb285f4825283b4a
Paul Fernando Gutierrez <paulgut2001804@gmail.com>
2022-11-30 01:24:07 -0500

    Cambio 2

--- sketch.js
+++ sketch.js
@@ -1,17 +1,25 @@
-let nPelotas = 100;
-let pelotas = [];
+let particulas = 1000;
+let pelotas = [2];
 
 function setup() {
+  background('rgba(0,0,0)');
   createCanvas(windowWidth, windowHeight);
-  for(let i = 0 ; i < nPelotas;i++){
+  for(let i = 0 ; i < particulas;i++){
     pelotas[i] = new RandomWalker(i);
   }
 }
 
 function draw() {
-  for(let i = 0 ; i < nPelotas;i++){
+  background (0, 0, 0)
+  for(let i = 5 ; i < particulas;i++){
     pelotas[i].update();
     pelotas[i].display();
+  }
+}
+function mouseClicked() {
+  for (let i = 0; i < 300; i++) {
+    let RandomWalker = new RandomWalker(mouseX, mouseY);
+    pelotas.push(RandomWalker);
   }
 }
 //___________Classes___________
@@ -20,21 +28,31 @@ function draw() {
 //___________Random Walker___________
 class RandomWalker{
   constructor(_nombre){
-    this.red = random(130, 250);
+    this.red = random(130, 30);
     this.green = random (50,255);
-    this.blue = random (100, 50);
+    this.blue = random (150, 150);
+
+    this.t = 0;
+    this.tSpeed = random(0.5);
+    this.noiseShift = random(100);
 
   this.pos= createVector(width/2, height/2);
-    this.speed = createVector(random(4,10), random (4,10));
-    this.diametro = random (10,50);
+    this.speed = createVector(random(-2,2), random (-2,2));
+    this.diametro = random (3,5);
     print('hola soy la pelota' + this.nombre);
 
   }
-  update(){
+  update(_t){
+
+    this.speed.rotate(map(noise(this.t + this.noiseShift), -1, 3, -0.1, 0.1));
     this.pos.add(this.speed);
+
+    this.t += this.tSpeed;
   }
   display(){
+    stroke('rgba(0,0,0,.2)');
+    strokeWeight(1);
     fill(this.red, this.green, this.blue)
-    ellipse(this.pos.x, this.pos.y, this.diametro, this.diametro);
+    rect(this.pos.x, this.pos.y, this.diametro, this.diametro);
   }
 }
